c813033272e3ae724de3c6363bfc3a5ea99c727e
JasperBroeker <jasper.broeker@gmx.de>
2016-06-09 13:58:17 +0200

    ADDED: DisplayLevelSeuqence

--- code/DisplayLevelSequence.asm
+++ code/DisplayLevelSequence.asm
@@ -1,14 +1,23 @@
+org 0h
+value equ 70h
+ljmp go
+
+org 0000bh
 showSequence:
-	mov R1,70h
-	mov P1, R1
-	mov R2, 71h
-	mov P1, R2
-	mov R3, 72h
-	mov P1, R3
-	mov R4, 73h
-	mov P1, R4
-	mov R5, 74h
-	mov P1, R5
-	mov R6, 75h
-	mov P1, R6
+	mov R1, value
+	mov P1, @R1
+	inc value
 	call showsequence
+	reti
+	
+go:
+	mov TMOD, #000000010b
+	mov TL0,#155
+	mov TL1, #155
+	mov TCON, #00010000b
+	mov ip, #0
+	setb EA
+	setb ET0
+
+haupt:
+sjmp haupt
